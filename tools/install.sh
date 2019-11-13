1faa2d9a067d51f4f2572ec47da53d6a9d676a90
EricKwok <erickwok404@gmail.com>
2019-11-13 20:08:41 +0800

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -67,15 +67,14 @@ main() {
   " ~/.zshrc > ~/.zshrc-omztemp
   mv -f ~/.zshrc-omztemp ~/.zshrc
 
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
+  # If this platform provides a "chsh" command (not Cygwin), do it, man!
+  if hash chsh >/dev/null 2>&1; then
+    printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+    chsh -s $(grep /zsh$ /etc/shells | tail -1)
+  # Else, suggest the user do so manually.
+  else
+    printf "I can't change your shell automatically because this system does not have chsh.\n"
+    printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
   fi
 
   printf "${GREEN}"
